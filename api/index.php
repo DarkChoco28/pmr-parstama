1111b51f293efed5ac3973b7876ee9c3a74679d4
DarkChoco28 <yarn8372@gmail.com>
2026-06-26 20:52:35 +0700

    fix env for vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,10 @@
 <?php
 
+// Kalau tidak ada .env, pakai .env.production (untuk Vercel)
+if (!file_exists(__DIR__ . '/../.env') && file_exists(__DIR__ . '/../.env.production')) {
+    copy(__DIR__ . '/../.env.production', __DIR__ . '/../.env');
+}
+
 // Entry point untuk Vercel serverless
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
